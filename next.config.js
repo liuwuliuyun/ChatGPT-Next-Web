32975856c14609dfdceca7f89611a37e5db9891e
Yun Liu <yunliu1@microsoft.com>
2023-03-27 16:56:29 +0800

    update to test

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    unoptimized: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
